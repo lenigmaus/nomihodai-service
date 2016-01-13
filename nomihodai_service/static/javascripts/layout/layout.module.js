611e8e7585397dc9d71f95dd12d11bd251d8f320
lenigmaus <laszlo@kardos.me>
2016-01-13 09:59:24 +0900

    search added to the API

--- nomihodai_service/static/javascripts/layout/layout.module.js
+++ nomihodai_service/static/javascripts/layout/layout.module.js
@@ -11,7 +11,7 @@
     .config(function($mdThemingProvider) {
 		$mdThemingProvider.theme('default')
 			.primaryPalette('blue', {
-				'default': '700', // by default use shade 400 from the pink palette for primary intentions
+				'default': '500', // by default use shade 400 from the pink palette for primary intentions
 				'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
 				'hue-2': '400', // use shade 600 for the <code>md-hue-2</code> class
 				'hue-3': '800' // use shade A100 for the <code>md-hue-3</code> class
